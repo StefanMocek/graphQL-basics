0c914036cc7ecee433445167e3b9a7b4c83dee7c
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-03 20:58:20 +0100

    packages and server fixed

--- server.js
+++ server.js
@@ -1,55 +1,22 @@
-const path = require('path');
 const express = require('express');
-
-const { ApolloServer } = require('@apollo/server');
-
+const { ApolloServer } = require('apollo-server-express');
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
-const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
-
-const schema = makeExecutableSchema({
-  typeDefs: typesArray
+const typesArray = loadFilesSync('**/*', {
+  extensions: ['graphql'],
 });
 
-const root = {
-  products: [
-    {
-      id: 'redshoe',
-      description: 'Red Shoe',
-      price: 42.12,
-      reviews: [],
-    },
-    {
-      id: 'bluejean',
-      description: 'Blue Jeans',
-      price: 55.55,
-      reviews: [],
-    }
-  ],
-  orders: [
-    {
-      date: '2005-05-05',
-      subtotal: 90.22,
-      items: [ 
-        {
-          product: {
-            id: 'redshoe',
-            description: 'Old Red Shoe',
-            price: 45.11,
-          },
-          quantity: 2,
-        }
-      ]
-    }
-  ]
-}
+const resolversArray = loadFilesSync('**/*', {
+  extensions: ['resolvers.js'],
+});
 
 async function startApolloServer() {
   const app = express();
 
   const schema = makeExecutableSchema({
-    typeDefs: typesArray
+    typeDefs: typesArray,
+    resolvers: resolversArray,
   });
 
   const server = new ApolloServer({
