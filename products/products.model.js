dd8b5cb1b897ceb2f26e6f244f4f3017e2db8176
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-03 20:44:52 +0100

    mutation added

--- products/products.model.js
+++ products/products.model.js
@@ -15,22 +15,51 @@ const products = [
 
 function getAllProducts() {
   return products;
-};
+}
 
 function getProductsByPrice(min, max) {
   return products.filter((product) => {
     return product.price >= min && product.price <= max;
   });
-};
+}
 
 function getProductById(id) {
   return products.find((product) => {
-    return product.id === id
+    return product.id === id;
   });
+}
+
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    price,
+    description,
+    reviews: []
+  };
+
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductById(id);
+
+  if (matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment,
+    };
+
+    matchedProduct.reviews.push(newProductReview);
+
+    return newProductReview;
+  }
 }
 
 module.exports = {
   getAllProducts,
   getProductsByPrice,
-  getProductById
+  getProductById,
+  addNewProduct,
+  addNewProductReview,
 };
