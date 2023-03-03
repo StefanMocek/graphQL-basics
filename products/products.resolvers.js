4ec6036c1eef048d40de4460c931dc1fa2a48da2
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-03 19:37:50 +0100

    add new functions which are getting the data

--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -4,6 +4,12 @@ module.exports = {
   Query: {
     products: () => {
       return productsModel.getAllProducts();
-    }
+    },
+    productsByPrice: (_, args) => {
+      return productsModel.getProductsByPrice(args.min, args.max);
+    },
+    productById: (_, args) => {
+      return productsModel.getProductById(args.id);
+    },
   }
 };
